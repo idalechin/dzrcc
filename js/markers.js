8cb34dc50de9649671e92893f133cb9a4503f594
Ilya Dalechin <dalechinilya@gmail.com>
2016-11-11 13:47:00 +0300

    Поправил markers.js

--- js/markers.js
+++ js/markers.js
@@ -27,21 +27,17 @@ function refreshMarkersArray(data) {
                 code: code
             });
 
-            getAddress(marker.position, setMarkerAddress);
-
-            function setMarkerAddress(a) {
-                marker.address = a;
-                if(code === ''||/^\d+$/i.test(code)){
-                    marker.title = (code === '') ? a : val.code + ") " + marker.address;
-                } else {
-                    marker.title = code.replace(/^\d+[,.-]?/g, code.match(/^\d+/i)+")");
-                }
-                marker.id = val.id;
-                markersIds.push(val.id);
-                markers.push(marker);
-                setMarkerListeners(marker);
-                markerListRefresh();
+            marker.address = val.data;
+            if(code === ''||/^\d+$/i.test(code)){
+                marker.title = (code === '') ? marker.address : val.code + ") " + marker.address;
+            } else {
+                marker.title = code.replace(/^\d+[.)-]?/g, code.match(/^\d+/i)+".");
             }
+            marker.id = val.id;
+            markersIds.push(val.id);
+            markers.push(marker);
+            setMarkerListeners(marker);
+            markerListRefresh();
         }
         dbMarkersIds.push(val.id);
     });
